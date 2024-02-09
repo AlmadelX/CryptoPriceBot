8a2b9c8efae637ee7de89067b1cfb7823897c07e
Matvey Alexeev <almadelx8@gmail.com>
2024-02-10 01:44:49 +0300

    add: texts

--- src/bot.ts
+++ src/bot.ts
@@ -1,11 +1,27 @@
 import { Bot } from 'grammy';
 
-export default function runBot(token: string) {
+import text from '../assets/text.json';
+
+async function setupBot(token: string): Promise<Bot> {
     const bot = new Bot(token);
+
+    await bot.api.setMyCommands([
+        { command: 'help', description: text['command:help:description'] },
+        {
+            command: 'price',
+            description: text['command:price:description'],
+        },
+    ]);
 
     bot.command('start', ctx => ctx.reply('Welcome!'));
     bot.on('message', ctx => ctx.reply('Pong'));
 
-    console.log('Bot started!');
-    bot.start().then(() => console.log('Bot finished!'));
+    return bot;
+}
+
+export default async function runBot(token: string): Promise<void> {
+    const bot = await setupBot(token);
+
+    console.log('Bot started');
+    bot.start().then(() => console.log('Bot finished'));
 }
